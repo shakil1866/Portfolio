71e7ddae9af67c6173f37f90e3963f37154ab813
SHAKIL AHAMMED <shakilahammed01234@gmail.com>
2024-07-22 10:48:03 +0530

    second

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: [ "Client",  "Sir/Madam"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
